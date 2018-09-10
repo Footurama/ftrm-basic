ee55122599ca88d467a69dc5ed0bb6267d78b70e
Jue <me@jue.yt>
2018-09-10 19:42:15 +0200

    Added stream writer

---
+++ to-writable.js
@@ -0,0 +1,17 @@
+function check (opts) {
+	if (opts.input.length !== 1) throw new Error('One input must be specified');
+	if (opts.output.length !== 0) throw new Error('No outputs can be specified');
+	if (typeof opts.stream !== 'object' || typeof opts.stream.write !== 'function' || typeof opts.stream.end !== 'function') throw new Error('Stream must be specified');
+	if (opts.format === undefined) opts.format = (v, ts) => `${new Date(ts).toISOString()}\t${v.toString()}\n`;
+}
+
+function factory (opts, input, output) {
+	input[0].on('update', (value, timestamp) => {
+		const line = opts.format(value, timestamp);
+		opts.stream.write(line);
+	});
+
+	return () => new Promise((resolve) => opts.stream.end(resolve));
+}
+
+module.exports = { check, factory };
